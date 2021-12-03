d328c7afcd7aa4c2e8f07fc694c0e4f7aaa248c8
linlurui <rockylin@qq.com>
2021-12-03 17:58:03 +0800

    Create AppConfig.java

---
+++ ccweb-core/src/main/java/ccait/ccweb/model/AppConfig.java
@@ -0,0 +1,53 @@
+package ccait.ccweb.model;
+
+import entity.query.Queryable;
+import entity.query.annotation.AutoIncrement;
+import entity.query.annotation.PrimaryKey;
+import entity.query.annotation.Tablename;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+@Tablename("${ccweb.app-config.table}")
+public class AppConfig extends Queryable<AppConfig> {
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @AutoIncrement
+    @PrimaryKey
+    private Integer id;
+    private String service;
+    private String key;
+    private String value;
+}
