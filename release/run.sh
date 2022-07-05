5c2c30a53632095c2ad631434da41be9ffb13c13
linlurui <rockylin@qq.com>
2022-07-05 15:36:42 +0800

    Modified   release/run.sh

--- release/run.sh
+++ release/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# nohup java -jar ccweb-start-1.0.0-SNAPSHOT.jar -Dwatchdocker=true >aftask.log 2>&1 &
-nohup java -Xms1024m -Xmx2048m -Xss512k -Duser.timezone=GMT+08 -jar ccweb-start-1.0.0-SNAPSHOT.jar >ccweb.log 2>&1 &
+#nohup java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/d/dump/ -Xms2g -Xmx2g -Xss256k -Xmn600m -XX:PermSize=100m -XX:MaxPermSize=256m -XX:MaxTenuringThreshold=15 -XX:SurvivorRatio=8 -XX:NewRatio=4 -XX:ParallelGCThreads=8 -XX:-DisableExplicitGC -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -Duser.timezone=GMT+08 -jar ccweb-start.jar >ccweb.log 2>&1 &
+#nohup java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/d/dump/ -Duser.timezone=GMT+08 -jar ccweb-start.jar >ccweb.log 2>&1 &
+#nohup java -XX:TieredStopAtLevel=1 -Xverify:none -Dspring.output.ansi.enabled=always -Dcom.sun.management.jmxremote -Dspring.jmx.enabled=true -Dspring.liveBeansView.mbeanDomain -Dspring.application.admin.enabled=true -Dfile.encoding=UTF-8 -Duser.timezone=GMT+08 -jar ccweb-start.jar
 echo $! > tpid
-echo Start ccweb-start-1.0.0-SNAPSHOT.jar Success!
+echo Start ccweb-start.jar Success!
 exit
