5e15926f2869a1278216de72297379d450388c10
linlurui <rockylin@qq.com>
2022-07-05 18:05:06 +0800

    Modified   release/run.sh

--- release/run.sh
+++ release/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-nohup java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/d/dump/ -Xms2g -Xmx2g -Xss256k -Xmn600m -XX:PermSize=100m -XX:MaxPermSize=256m -XX:MaxTenuringThreshold=15 -XX:SurvivorRatio=8 -XX:NewRatio=4 -XX:ParallelGCThreads=8 -XX:-DisableExplicitGC -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -Duser.timezone=GMT+08 -jar ccweb-start.jar >ccweb.log 2>&1 &
+nohup java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/d/dump/ -Xms1536m -Xmx1536m -Xss256k -Xmn1000m -XX:PermSize=100m -XX:MaxPermSize=256m -XX:MaxTenuringThreshold=10 -XX:PretenureSizeThreshold=1m -XX:SurvivorRatio=8 -XX:NewRatio=4 -XX:ParallelGCThreads=4 -XX:-DisableExplicitGC -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseParNewGC -Duser.timezone=GMT+08 -jar ccweb-start.jar >ccweb.log 2>&1 &
 #nohup java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/d/dump/ -Duser.timezone=GMT+08 -jar ccweb-start.jar >ccweb.log 2>&1 &
-#nohup java -XX:TieredStopAtLevel=1 -Xverify:none -Dspring.output.ansi.enabled=always -Dcom.sun.management.jmxremote -Dspring.jmx.enabled=true -Dspring.liveBeansView.mbeanDomain -Dspring.application.admin.enabled=true -Dfile.encoding=UTF-8 -Duser.timezone=GMT+08 -jar ccweb-start.jar
+#nohup java -XX:TieredStopAtLevel=1 -Xverify:none -Dspring.jmx.enabled=true -Dspring.liveBeansView.mbeanDomain -Dfile.encoding=UTF-8 -Duser.timezone=GMT+08 -jar ccweb-start.jar
 echo $! > tpid
 echo Start ccweb-start.jar Success!
 exit
